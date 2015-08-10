42b9a58daa7775ae2b4bb0f3623281f79d04c51e
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-08-10 17:07:32 +0100

    setup typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = "Ferdinand van Wyk",
     author_email = 'ferdinandvwyk@gmail.com',
     description = "PyFilm: easily create films of 1D and 2D NumPy arrays.",
-    long_description = "PyFilm is a package which enables the quick creation of animations of NumPy arrays. It does this by writing each time step as a separate image and using either ffmpeg or avconv to stitch the images together into an animation. The package is designed to be very easy to use, and allow animations to be created with a single line."
+    long_description = "PyFilm is a package which enables the quick creation of animations of NumPy arrays. It does this by writing each time step as a separate image and using either ffmpeg or avconv to stitch the images together into an animation. The package is designed to be very easy to use, and allow animations to be created with a single line.",
     license = "GNU",
     keywords = ['film', 'animation', 'numpy', 'ffmpeg', 'avconv'],
     packages=['pyfilm', 'tests', 'docs'],
