c18e829c16b3df631a243bc168f7dd0e250f1474
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-08-10 17:12:01 +0100

    remove tests, docs from packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description = "PyFilm is a package which enables the quick creation of animations of NumPy arrays. It does this by writing each time step as a separate image and using either ffmpeg or avconv to stitch the images together into an animation. The package is designed to be very easy to use, and allow animations to be created with a single line.",
     license = "GNU",
     keywords = ['film', 'animation', 'numpy', 'ffmpeg', 'avconv'],
-    packages=['pyfilm', 'tests', 'docs'],
+    packages=['pyfilm'],
     url = 'https://github.com/ferdinandvwyk/pyfilm.git',
     classifiers=[
     "Development Status :: 3 - Alpha"
