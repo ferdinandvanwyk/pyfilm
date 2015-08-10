0e496b0f24de6a8aeb35b93cd885d471bb732ae5
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-08-10 16:49:55 +0100

    remove README from setup

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "pyfilm",
     version = "0.1.0",
@@ -18,7 +11,6 @@ setup(
     license = "GNU",
     keywords = ['film', 'animation', 'array', 'numpy'],
     packages=['pyfilm', 'tests', 'docs'],
-    long_description=read('README.rst'),
     url = 'git@github.com:ferdinandvwyk/pyfilm.git',
     classifiers=[],
 )
